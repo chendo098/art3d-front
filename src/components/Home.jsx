1be5ff79f309c62c7538bc42bd495bf88a3999e8
Rosendo Alves <rosendoalves05@gmail.com>
2022-10-18 20:53:35 -0300

    spinner created

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LoadingSpinner from './LoadingSpinner'
+
+const Home = () => {
+  return (
+    <>
+    <div>Home</div>
+    <LoadingSpinner/>
+    </>
+  )
+}
+
+export default Home
